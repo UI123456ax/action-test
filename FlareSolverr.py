a71a367279efc3a7758495ad8f4a98155c9707a9
UI123456ax <ui123456ax@163.com>
2025-08-08 00:55:39 +0800

    重构 FlareSolverr.py，移除冗余代码并简化请求逻辑

--- FlareSolverr.py
+++ FlareSolverr.py
@@ -1,32 +1,15 @@
-import cloudscraper
-
-
-scraper = cloudscraper.create_scraper()
-resp = scraper.get('https://18comic.vip').text
-print(resp[:1000])  # Print the first 1000 characters of the response
-
-import json
 import requests
 
-def cloudflare(self, xxxx):
-    print("破盾中...")
-    urlServer = "https://yadtenumogxm.eu-central-1.clawcloudrun.com/v1"
-    payload = json.dumps({
-        "cmd": "request.get",
-        "url": 'https://18comic.vip',
-        "maxTimeout": 160000
-    })
-    headers = {
-        'Content-Type': 'application/json'
-    }
-    response = requests.post(urlServer, headers=headers, data=payload)
-    # print(response.status_code)
-    # print(response.json()['solution']['cookies'])
-    if response.status_code == 200:
-        userAgent = response.json()['solution']['userAgent']
-        for item in response.json()['solution']['cookies']:
-            self.cookies[item["name"]] = item["value"]
-        self.UA = userAgent
-        print("破盾成功")
-        return response
-    print("绕过5秒盾错误！！！")
+url = "http://localhost:8191/v1"
+headers = {"Content-Type": "application/json"}
+data = {
+    "cmd": "request.get",
+    # "url": "https://ip.sb/",
+    "url": "https://18comic.vip/",
+    "maxTimeout": 160000
+}
+
+response = requests.post(url, headers=headers, json=data)
+print("Status:", response.json().get('status', {}))
+print("Status Code:", response.status_code)
+print("FlareSolverr message:", response.json().get('message', {}))
