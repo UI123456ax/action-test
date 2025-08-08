8b44da7c66a93e644d464e2acf235bf4541fe964
UI123456ax <ui123456ax@163.com>
2025-08-08 13:28:50 +0800

    完善 FlareSolverr.py，添加对成功响应的 cookie 和 userAgent 的处理

--- FlareSolverr.py
+++ FlareSolverr.py
@@ -13,3 +13,10 @@ response = requests.post(url, headers=headers, json=data)
 print("Status:", response.json().get('status', {}))
 print("Status Code:", response.status_code)
 print("FlareSolverr message:", response.json().get('message', {}))
+
+cookies = {}
+if response.status_code == 200:
+    print(response.json())
+    for item in response.json()['solution']['cookies']:
+        cookies[item["name"]] = item["value"]
+    userAgent = response.json()['solution']['userAgent']
