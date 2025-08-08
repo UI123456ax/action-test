01500b6ec5ab0b8ff952e85d51e818ffe553e386
UI123456ax <ui123456ax@163.com>
2025-08-08 14:02:36 +0800

    添加 jmcomic 模块并更新 FlareSolverr.py 以支持用户代理和 cookie 处理；更新 requirements.txt 以包含新依赖

--- FlareSolverr.py
+++ FlareSolverr.py
@@ -1,4 +1,5 @@
 import requests
+import jmcomic
 
 url = "http://localhost:8191/v1"
 headers = {"Content-Type": "application/json"}
@@ -15,8 +16,14 @@ print("Status Code:", response.status_code)
 print("FlareSolverr message:", response.json().get('message', {}))
 
 cookies = {}
+session = requests.session()
 if response.status_code == 200:
     print(response.json())
     for item in response.json()['solution']['cookies']:
         cookies[item["name"]] = item["value"]
     userAgent = response.json()['solution']['userAgent']
+    session.headers.update({"User-Agent": userAgent})
+    session.cookies.update(cookies)
+
+if __name__ == '__main__':
+    jmcomic.Main(username='test', password='123666')
